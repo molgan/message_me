a659225749c8ad6c497b395d48b287fb3bb8ba55
molgan <olga.matsuga@gmail.com>
2019-08-01 22:58:15 +0000

    Add User and Message models, add messages view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "molgan", password: "password")
+User.create(username: "Evgeniy", password: "password")
+User.create(username: "Alex", password: "password")
+User.create(username: "Kate", password: "password")
+User.create(username: "Denis", password: "password")
