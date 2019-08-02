90289be7570f5b23d6be35c20baf3fb69713e0e2
molgan <olga.matsuga@gmail.com>
2019-08-02 23:33:25 +0000

    Change production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,8 +45,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.url = 'wss://messageme-molgan.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://messageme-molgan.herokuapp.com/' ]
+  config.action_cable.allowed_request_origins = [ 'https://messageme-molgan.herokuapp.com/', 'http://messageme-molgan.herokuapp.com/' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
